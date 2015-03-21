bad8cf8da872a2cc9c80a4b10f983667f97d303f
Marcus Breese <mbreese@stanford.edu>
2015-03-20 18:07:09 -0700

    updated to new cmdlinej, now main server method joins jobqueue and socketlistener threads...

---
+++ src/java/io/compgen/sjq/server/SJQServerException.java
@@ -0,0 +1,14 @@
+package io.compgen.sjq.server;
+
+public class SJQServerException extends Exception {
+
+	public SJQServerException(String s) {
+		super(s);
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3492708882884708204L;
+
+}
